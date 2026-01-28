b95b5c92f0d80eaae26d64d9f93e98d77b1f8f13
Bailey Emekoba <chidalub@gmail.com>
2026-01-28 17:19:03 +0000

    website title is newtaxlaws_ng

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'TaxClarity NG',
+  title: 'newtaxlaws_ng',
   description: 'See how the new tax laws in Nigeria affect your money.',
 };
 
